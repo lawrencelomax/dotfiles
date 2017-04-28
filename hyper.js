2ee1c7f1a74028f84a2ba5afd9171aef41c92763
lawrencelomax <lawrencelomax@fb.com>
2017-04-28 11:55:59 +0100

    hyper padding

--- hyper.js
+++ hyper.js
@@ -40,7 +40,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '0px 8px 0px 8px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
